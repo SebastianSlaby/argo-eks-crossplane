69fc3f49c90e5240c445b373ddb57c74c7d570ff
Sebastian Słaby <sebastianslaby98@gmail.com>
2024-10-28 00:38:54 +0100

    LB controller IRSA

--- terraform/modules/argo/irsa-lb.tf
+++ terraform/modules/argo/irsa-lb.tf
@@ -13,5 +13,5 @@ module "lb_controller_irsa_role" {
 
 resource "aws_iam_role_policy_attachment" "lb_controller" {
     policy_arn = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
-    role = module.lb_controller_irsa_role.iam_role_arn
+    role = module.lb_controller_irsa_role.iam_role_name
 }
